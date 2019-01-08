d6ff803466f24fa10ca25e3092e85ba429690b4f
chakradeb <debarun.chakraborty96@gmail.com>
2019-01-08 15:07:33 +0530

    Add user model

---
+++ models/user.go
@@ -0,0 +1,23 @@
+package models
+
+type User struct {
+	name string
+	todos []*Todo
+}
+
+func NewUser(name string) *User {
+	return &User{name: name}
+}
+
+func (user *User) AddTodo(name string, desc string) {
+	user.todos = append(user.todos, NewTodo(name, desc))
+}
+
+func (user *User) UpdateTodo(index int, updatedName string, updatedDesc string) {
+	user.todos[index].ChangeName(updatedName)
+	user.todos[index].ChangeDesc(updatedDesc)
+}
+
+func (user *User) RemoveTodo(index int) {
+	user.todos = append(user.todos[:index], user.todos[index+1:]...)
+}
---
+++ models/user_test.go
@@ -0,0 +1,72 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user := NewUser("Happy User")
+	expectedUser := &User{
+		name: "Happy User",
+	}
+
+	assert.Equal(t, expectedUser, user, "Invalid user")
+}
+
+func TestUser_AddTodo(t *testing.T) {
+	user := NewUser("Happy User")
+	user.AddTodo("Todo 1", "This is a simple todo")
+
+	todo := &Todo{
+		name:"Todo 1",
+		description: "This is a simple todo",
+	}
+
+	expectedUser := &User{
+		name:"Happy User",
+		todos: []*Todo{todo},
+	}
+	assert.Equal(t, expectedUser, user, "Invalid user todo")
+}
+
+func TestUser_UpdateTodo(t *testing.T) {
+	user := NewUser("Happy User")
+	user.AddTodo("Todo 1", "This is a simple todo")
+	user.UpdateTodo(0, "Updated name", "This is a updated description")
+
+	todo := &Todo{
+		name:"Updated name",
+		description: "This is a updated description",
+	}
+
+	expectedUser := &User{
+		name:"Happy User",
+		todos: []*Todo{todo},
+	}
+	assert.Equal(t, expectedUser, user, "Invalid user todo")
+}
+
+func TestUser_RemoveTodo(t *testing.T) {
+	user := NewUser("Happy User")
+	user.AddTodo("Todo 1", "This is a simple todo")
+	user.AddTodo("Todo 2", "This is a simple todo")
+	user.AddTodo("Todo 3", "This is a simple todo")
+	user.RemoveTodo(1)
+
+	todo1 := &Todo{
+		name:"Todo 1",
+		description: "This is a simple todo",
+	}
+	todo2 := &Todo{
+		name:"Todo 3",
+		description: "This is a simple todo",
+	}
+
+	expectedUser := &User{
+		name:"Happy User",
+		todos: []*Todo{todo1, todo2},
+	}
+	assert.Equal(t, expectedUser, user, "Invalid user todo")
+}
